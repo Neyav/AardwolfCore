4e4e0c34ba3522c99d5c4cad20a6c2f46893cd05
Chris Laverdure <greghealburn@hotmail.com>
2025-02-26 10:52:50 -0700

    Add a gamesession class and fix texture facing for quad rendering.

---
+++ gamesession.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AardwolfCore
+{
+    class gamesession
+    {
+        private int _mapnumber;
+        private int _difficulty;
+        private bool _isSOD;
+        private dataHandler _dataHandler;
+
+        public gamesession(bool isSOD, int mapnumber, int difficulty)
+        {
+            this._mapnumber = mapnumber;
+            this._difficulty = difficulty;
+            this._isSOD = isSOD;
+
+            this._dataHandler = new dataHandler();
+            _dataHandler.loadAllData(_isSOD);
+        }
+    }
+}
