503f92f65525660a262696a9a541befa638dbf89
Chris Laverdure <greghealburn@hotmail.com>
2025-02-26 13:44:44 -0700

    Start the move to use gamesession to manage the gamestate.

--- gamesession.cs
+++ gamesession.cs
@@ -6,21 +6,48 @@ using System.Threading.Tasks;
 
 namespace AardwolfCore
 {
-    class gamesession
+    public class gamesession
     {
         private int _mapnumber;
         private int _difficulty;
         private bool _isSOD;
         private dataHandler _dataHandler;
+        
 
-        public gamesession(bool isSOD, int mapnumber, int difficulty)
+        public int getLevels()
         {
-            this._mapnumber = mapnumber;
-            this._difficulty = difficulty;
+            return _dataHandler.getLevels();
+        }
+        public string getLevelName(int level)
+        {
+            return _dataHandler.getLevelName(level);
+        }
+
+        public void loadLevel(int level, int difficulty)
+        {
+            
+        }
+
+        // Not necessary for a game, just for testing
+        public VSWAPHeader TEST_getVSWAPHeader()
+        {
+            return _dataHandler.getVSWAPHeader;
+        }
+        public dataHandler TEST_getDataHandler()
+        {
+            return _dataHandler;
+        }
+        public gamesession(bool isSOD)
+        {
+            this._mapnumber = 0;
+            this._difficulty = 0;
             this._isSOD = isSOD;
 
             this._dataHandler = new dataHandler();
             _dataHandler.loadAllData(_isSOD);
+
+            _dataHandler.parseLevelData();
+            _dataHandler.prepareVSWAP();            
         }
     }
 }
