aae26b19a1975d3570593a7969b283380e034b28
Chris Laverdure <greghealburn@hotmail.com>
2025-07-02 11:05:41 -0600

    This still needs to be updated for animation, but at least you can spawn Guards and they appear in the view port now.

---
+++ AIActor.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AardwolfCore
+{
+    public class AIActor
+    {
+        public int Health { get; set; }
+        public int MaxHealth { get; set; }
+        public float WidthPosition { get; set; }
+        public float HeightPosition { get; set; }
+
+    }
+}
