457ef49067299df329db029d30b4bb8c71e47ff3
Chris Laverdure <greghealburn@hotmail.com>
2025-07-06 21:13:04 -0600

    Getting ready for the event system.

--- Actors/Actor.cs
+++ Actors/Actor.cs
@@ -13,6 +13,9 @@ namespace AardwolfCore.Actors
         public int Angle { get; set; }
         public float WidthPosition { get; set; }
         public float HeightPosition { get; set; }
+        // SkillSetting is used to determine the difficulty or skill level of the actor
+        // This is static so it can be set globally for all actors.
+        public static int SkillSetting { get; set; } = 0; // Default skill setting
 
     }
 }
---
+++ Actors/SystemActor.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AardwolfCore.Actors
+{
+    // SystemActor is the phantom actor that will be used to access the event system.
+    // It will be responsibile for initalizing the event system and handling events.
+    internal class SystemActor : Actor
+    {
+        public SystemActor()
+        {
+            
+        }
+    }
+}
